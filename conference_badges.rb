fd9852826469706e4d07fc81bb109bfd008efe7d
Bperez4197 <bryceperez31@gmail.com>
2017-11-29 09:23:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-
 def batch_badge_creator(array)
    badge_messages = []
    array.each do |name| badge_messages << badge_maker(name)
