34d29b2565b5dbcea3090adcb9d823fbc7a542e3
Bperez4197 <bryceperez31@gmail.com>
2017-11-29 09:22:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(attendees)
-  room = [1,2,3,4,5,6,7]
-  speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
-  message = []
-  speakers.each do |number| message <<
-  "Hello, #{speakers}! You'll be assigned to room #{room}!"
-end
+  
 end
