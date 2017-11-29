7a2b9dab7cba064f8c95bd619857b8879152cc8d
Bperez4197 <bryceperez31@gmail.com>
2017-11-29 09:18:57 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,20 @@
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
 speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
 def batch_badge_creator(array)
- badge_messages = []
- array.each do |name| badge_messages << badge_maker(name)
+   badge_messages = []
+   array.each do |name| badge_messages << badge_maker(name)
  end
  badge_messages
+end
+
+def assign_rooms(attendees)
+  room = [1,2,3,4,5,6,7]
+  speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+  speakers,room.each do |number| 
+  "Hello, #{speakers}! You'll be assigned to room #{room}!"
+end
 end
